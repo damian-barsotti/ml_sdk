6b1f3af7126b0ab56ae3c9ae178b2973e5d6d097
BuildTools <damian@famaf.unc.edu.ar>
2024-05-22 16:25:47 -0300

    Refactor

--- communication/__init__.py
+++ communication/__init__.py
@@ -43,19 +43,20 @@ class WorkerInterface(ProducerConsumerInterface):
 class DispatcherInterface(ProducerConsumerInterface):
 
     def dispatch(self, method, **kwargs):
+
+        def get_reply(key):
+            key, result = self._consume(key)
+            return result
+
         key = uuid.uuid4().hex
         kwargs['method'] = method
         self._produce(kwargs, key)
-        result = self._get_reply(key)
+        result = get_reply(key)
         return result
 
     def broadcast(self, method, **kwargs):
         kwargs['method'] = method
         return self._broadcast(kwargs)
-
-    def _get_reply(self, key):
-        key, result = self._consume(key)
-        return result
 
     @abstractmethod
     def _broadcast(self, message):
