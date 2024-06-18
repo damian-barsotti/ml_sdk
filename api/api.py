90a270ebfa114ebcd471edfa0abdf265082f4362
BuildTools <damian@famaf.unc.edu.ar>
2024-06-18 12:46:47 -0300

    Test code

--- api/api.py
+++ api/api.py
@@ -17,121 +17,8 @@ from ml_sdk.io import (
 )
 from ml_sdk.io.version import (ModelVersion, ModelDescription,
                                VersionID, AvailableModels)
-# Security
-##########
-from datetime import datetime, timedelta, timezone
-import jwt
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from jwt.exceptions import InvalidTokenError
-from passlib.context import CryptContext
-from pydantic import BaseModel
 
-# to get a string like this run:
-# openssl rand -hex 32
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-fake_users_db = {
-    "johndoe": {
-        "username": "johndoe",
-        "full_name": "John Doe",
-        "email": "johndoe@example.com",
-        "hashed_password": (
-            "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW"),
-        "disabled": False,
-    }
-}
-
-
-class Token(BaseModel):
-    access_token: str
-    token_type: str
-
-
-class TokenData(BaseModel):
-    username: str | None = None
-
-
-class User(BaseModel):
-    username: str
-    email: str | None = None
-    full_name: str | None = None
-    disabled: bool | None = None
-
-
-class UserInDB(User):
-    hashed_password: str
-
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-oauth2_scheme = OAuth2PasswordBearer(
-    tokenUrl="acl_imdb_sentiment_analysis/token")
-
-
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
-
-
-def get_password_hash(password):
-    return pwd_context.hash(password)
-
-
-def get_user(db, username: str):
-    if username in db:
-        user_dict = db[username]
-        return UserInDB(**user_dict)
-
-
-def authenticate_user(fake_db, username: str, password: str):
-    user = get_user(fake_db, username)
-    if not user:
-        return False
-    if not verify_password(password, user.hashed_password):
-        return False
-    return user
-
-
-def create_access_token(data: dict, expires_delta: timedelta | None = None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.now(timezone.utc) + expires_delta
-    else:
-        expire = datetime.now(timezone.utc) + timedelta(minutes=15)
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
-
-
-async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise credentials_exception
-        token_data = TokenData(username=username)
-    except InvalidTokenError:
-        raise credentials_exception
-    user = get_user(fake_users_db, username=token_data.username)
-    if user is None:
-        raise credentials_exception
-    return user
-
-
-async def get_current_active_user(
-    current_user: Annotated[User, Depends(get_current_user)],
-):
-    if current_user.disabled:
-        raise HTTPException(status_code=400, detail="Inactive user")
-    return current_user
-
-
-# End security
+from ml_sdk.api.auth import Auth
 
 logger = logging.getLogger()
 
@@ -171,6 +58,10 @@ class MLAPI:
         # API Routes
         self.router = APIRouter()
         self._add_routes()
+
+        self.auth = Auth(self.MODEL_NAME, self.router)
+
+        self.oauth2_scheme = self.auth.oauth2_scheme
 
     def _validate_instance(self):
         assert self.INPUT_TYPE is not None, "You have to setup an INPUT_TYPE"
@@ -213,19 +104,7 @@ class MLAPI:
                                   methods=["POST"],
                                   response_model=ModelVersion)
         self.router.add_api_route("/version",
-                                  self.get_version,
-                                  methods=["GET"])
-        # Security
-        self.router.add_api_route("/token",
-                                  self.login_for_access_token,
-                                  methods=["POST"],
-                                  response_model=Token)
-        self.router.add_api_route("/users/me/",
-                                  self.read_users_me,
-                                  methods=["GET"],
-                                  response_model=User)
-        self.router.add_api_route("/users/me/items/",
-                                  self.read_own_items,
+                                  self.get_version(),
                                   methods=["GET"])
 
     # VIEWS
@@ -301,19 +180,24 @@ class MLAPI:
         self._async_train(background_tasks, job=job, items=items)
         return job
 
-    def get_version(self,
-                    token: Annotated[str, Depends(oauth2_scheme)]
-                    ) -> AvailableModels:
+    def get_version(self):
 
-        try:
-            result = self.connector.dispatch('available_versions')
-        except ValueError:
-            raise HTTPException(
-                status_code=404,
-                detail="Service timeout for available_versions")
+        oauth2_scheme = self.oauth2_scheme
 
-        logger.info(f"get_version {result=}")
-        return result
+        def _inner(token: Annotated[str, Depends(oauth2_scheme)]
+                   ) -> AvailableModels:
+
+            try:
+                result = self.connector.dispatch('available_versions')
+            except ValueError:
+                raise HTTPException(
+                    status_code=404,
+                    detail="Service timeout for available_versions")
+
+            logger.info(f"get_version {result=}")
+            return result
+
+        return _inner
 
     def post_version(self, version_id: VersionID):
         input_ = ModelVersion(version=version_id)
@@ -385,35 +269,3 @@ class MLAPI:
             self.database.update_train_job(job=job, version=model_version)
 
         background_tasks.add_task(_inner, self.database, job, items)
-
-    # Security
-    async def login_for_access_token(
-        self,
-        form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
-    ) -> Token:
-        user = authenticate_user(fake_users_db,
-                                 form_data.username,
-                                 form_data.password)
-        if not user:
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="Incorrect username or password",
-                headers={"WWW-Authenticate": "Bearer"},
-            )
-        access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-        access_token = create_access_token(
-            data={"sub": user.username}, expires_delta=access_token_expires
-        )
-        return Token(access_token=access_token, token_type="bearer")
-
-    async def read_users_me(
-        self,
-        current_user: Annotated[User, Depends(get_current_active_user)],
-    ):
-        return current_user
-
-    async def read_own_items(
-        self,
-        current_user: Annotated[User, Depends(get_current_active_user)],
-    ):
-        return [{"item_id": "Foo", "owner": current_user.username}]
